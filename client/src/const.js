f99842a5657d0a2c7167b46f5631dfea6b5889f0
Hans Vertriest <hans.vertriest@gmail.com>
2021-08-06 19:10:29 +0200

    updated consts

--- client/src/const.js
+++ client/src/const.js
@@ -2,5 +2,5 @@
 
 export const ROUTER_HASH = '#!';
 export const ROUTER_DEFAULT_PAGE = '';
-// export const BASE_URL = 'http://ea15s003.ugent.be:8000';
-export const BASE_URL = 'http://0.0.0.0:8000';
+// export const BASE_URL = 'http://157.193.244.135:8000'; // for prod
+export const BASE_URL = '0.0.0.0:8000'; // For dev
