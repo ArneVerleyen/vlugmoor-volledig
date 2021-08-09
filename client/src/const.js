b06661e999deeadbf3f290cd79344167bbe1794e
Hans Vertriest <hans.vertriest@gmail.com>
2021-08-09 21:35:10 +0200

    exposed local mongo

--- client/src/const.js
+++ client/src/const.js
@@ -2,5 +2,5 @@
 
 export const ROUTER_HASH = '#!';
 export const ROUTER_DEFAULT_PAGE = '';
-export const BASE_URL = 'http://157.193.244.135:8000'; // for prod
-// export const BASE_URL = '0.0.0.0:8000'; // For dev
+export const BASE_URL = 'http://157.193.244.135:8000'; // for production
+// export const BASE_URL = '0.0.0.0:8000'; // For development
